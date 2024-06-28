65707263371451de5de587cf7b281a7e8fe03333
Leandro Blas <magnos_le@hotmail.com>
2024-06-28 11:50:11 -0300

    adicionando aula 28-06

---
+++ 28-06-24/exercicio1.js
@@ -0,0 +1,3 @@
+for (let x = 0; x <= 10; x++){
+    console.log(`esta é a ${x} ª vez que está frase é exibida no console: `);
+}
---
+++ 28-06-24/for2.js
@@ -0,0 +1,8 @@
+let frutas = ["maça", "banana", "pera"]
+
+//console.table(frutas)
+console.table(frutas)
+
+for (let i = 0; i < frutas.length; i++) {
+    console.log(`-${frutas[i]}`);
+}
